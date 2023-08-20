b4bdeeb763a89301a321040a1c9047018ed78c7e
Divyansh <divyanshraj0408@gmail.com>
2023-08-20 13:01:38 +0530

    add input validation ✨

--- client/src/shared/Input/Input.tsx
+++ client/src/shared/Input/Input.tsx
@@ -1,3 +1,5 @@
+import { useReducer } from "react";
+
 interface InputProps {
   placeholder?: string;
   type: string;
@@ -6,13 +8,51 @@ interface InputProps {
   id?: string;
   rows?: number;
 }
+
+const inputReducer = (state: any, action: any) => {
+  switch (action.type) {
+    case "CHANGE":
+      return {
+        ...state,
+        value: action.val,
+        isValid: true,
+      };
+
+    default:
+      return state;
+  }
+};
 const Input = (props: InputProps) => {
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: "",
+    isValid: false,
+  });
+  const changeHandler = (event) => {
+    dispatch({ type: "CHANGE", val: event.target.value });
+  };
+
   const element =
     props.element === "input" ? (
-      <input id={props.id} type={props.type} placeholder={props.placeholder} />
+      <input
+        id={props.id}
+        type={props.type}
+        placeholder={props.placeholder}
+        onChange={changeHandler}
+        value={inputState.value}
+      />
     ) : (
-      <textarea id={props.id} rows={props.rows || 3} />
+      <textarea
+        id={props.id}
+        rows={props.rows || 3}
+        onChange={changeHandler}
+        value={inputState.value}
+      />
     );
-  return <>{element}</>;
+  return (
+    <>
+      {element}
+      {inputState.isValid ? null : <p>{props.placeholder} is not valid</p>}
+    </>
+  );
 };
 export default Input;
