e63c8a6a72962a060e6abc7e30fb7704f6346344
Divyansh <divyanshraj0408@gmail.com>
2023-08-24 21:20:36 +0530

    add validation to forms 💚

--- client/src/shared/Form/Form.tsx
+++ client/src/shared/Form/Form.tsx
@@ -3,6 +3,7 @@ import "../../classes/components/cards/cards.css";
 import "./Form.css";
 import Input from "../Input/Input";
 import Button from "../button/Button";
+import { VALIDATOR_REQUIRE } from "../util/validator";
 
 interface Props {
   handleClick: () => void;
@@ -16,9 +17,15 @@ const Form = (props: Props) => {
       <form className="card form">
         <h2 className="form__name">{props.name}</h2>
         {props.noOfInputs.map((input) => (
-          <Input key={input} element="input" placeholder={input} type="text" />
+          <Input
+            key={input}
+            element="input"
+            placeholder={input}
+            type="text"
+            validators={[VALIDATOR_REQUIRE()]}
+          />
         ))}
-        <Button inverse>Click</Button>
+        <Button inverse>{props.name}</Button>
       </form>
     </>
   );
--- client/src/shared/Input/Input.tsx
+++ client/src/shared/Input/Input.tsx
@@ -1,5 +1,6 @@
 import { useReducer } from "react";
 
+import { validate } from "../util/validator";
 import "./Input.css";
 interface InputProps {
   placeholder?: string;
@@ -8,6 +9,7 @@ interface InputProps {
   element?: string;
   id?: string;
   rows?: number;
+  validators?: any[];
 }
 
 const inputReducer = (state: any, action: any) => {
@@ -16,8 +18,14 @@ const inputReducer = (state: any, action: any) => {
       return {
         ...state,
         value: action.val,
-        isValid: true,
+        isValid: validate(action.val, action.validators),
       };
+    case "TOUCH": {
+      return {
+        ...state,
+        isTouched: true,
+      };
+    }
 
     default:
       return state;
@@ -26,10 +34,21 @@ const inputReducer = (state: any, action: any) => {
 const Input = (props: InputProps) => {
   const [inputState, dispatch] = useReducer(inputReducer, {
     value: "",
+    isTouched: false,
     isValid: false,
   });
   const changeHandler = (event) => {
-    dispatch({ type: "CHANGE", val: event.target.value });
+    dispatch({
+      type: "CHANGE",
+      val: event.target.value,
+      validators: props.validators,
+    });
+  };
+
+  const touchHandler = () => {
+    dispatch({
+      type: "TOUCH",
+    });
   };
 
   const element =
@@ -39,6 +58,7 @@ const Input = (props: InputProps) => {
         type={props.type}
         placeholder={props.placeholder}
         onChange={changeHandler}
+        onBlur={touchHandler}
         value={inputState.value}
       />
     ) : (
@@ -46,18 +66,22 @@ const Input = (props: InputProps) => {
         id={props.id}
         rows={props.rows || 3}
         onChange={changeHandler}
+        onBlur={touchHandler}
         value={inputState.value}
       />
     );
   return (
-    <>
+    <div
+      className={`form-control ${
+        !inputState.isValid && inputState.isTouched && "form-control--invalid"
+      }`}
+    >
+      {/* <label htmlFor={props.id}>{props.placeholder}</label> */}
       {element}
-      {inputState.isValid ? null : (
-        <span className="warning_message">
-          {props.placeholder} is not valid ⚠️
-        </span>
+      {!inputState.isValid && inputState.isTouched && (
+        <p>{props.placeholder} is not valid ⚠️</p>
       )}
-    </>
+    </div>
   );
 };
 export default Input;
