1c515fb57dfa21362adf0811cdb9c34640aec3ca
Divyansh <divyanshraj0408@gmail.com>
2023-09-12 22:47:41 +0530

    fix bug: State update in auth.js  🐛

--- client/src/landingPage/pages/Auth.tsx
+++ client/src/landingPage/pages/Auth.tsx
@@ -3,6 +3,7 @@ import Input from "../../shared/Input/Input";
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
 } from "../../shared/util/Validator";
 import Button from "../../shared/button/Button";
 import Card from "../../shared/card/Card";
@@ -12,7 +13,7 @@ import "./Auth.css";
 
 const Auth = () => {
   const [isLoginMode, setIsLoginMode] = useState(true);
-  const [formState, inputHandler] = useForm(
+  const [formState, inputHandler, setFormData] = useForm(
     {
       email: {
         value: "",
@@ -26,9 +27,29 @@ const Auth = () => {
     false
   );
 
-  const switchModeHandler = () => {
-    setIsLoginMode((prevMode) => !prevMode);
-  };
+  // const switchModeHandler = () => {
+  //   if (!isLoginMode) {
+  //     setFormData(
+  //       {
+  //         ...formState.inputs,
+  //         name: undefined,
+  //       },
+  //       formState.inputs.email.isValid && formState.inputs.password.isValid
+  //     );
+  //   } else {
+  //     setFormData(
+  //       {
+  //         ...formState.inputs,
+  //         name: {
+  //           value: "",
+  //           isValid: false,
+  //         },
+  //       },
+  //       false
+  //     );
+  //   }
+  //   setIsLoginMode((prevMode) => !prevMode);
+  // };
   const authSubmitHandler = (event: any) => {
     event.preventDefault();
     console.log(formState.inputs);
@@ -39,28 +60,47 @@ const Auth = () => {
         <Card>
           <h2>Login required</h2>
           <form onSubmit={authSubmitHandler}>
+            {/* {!isLoginMode && (
+              <Input
+                element="input"
+                id="Name"
+                type="text"
+                placeholder="Name"
+                validators={[VALIDATOR_REQUIRE()]}
+                onInput={inputHandler}
+              />
+            )} */}
             <Input
               element="input"
               id="email"
               type="email"
-              placeholder="email"
+              placeholder="Email"
               validators={[VALIDATOR_EMAIL()]}
               onInput={inputHandler}
+              label="Email"
+              errorText="Please enter a valid email address"
             />
+
             <Input
               element="input"
               id="password"
               type="password"
               placeholder="password"
-              validators={[VALIDATOR_MINLENGTH(5)]}
+              validators={[VALIDATOR_MINLENGTH(6)]}
               onInput={inputHandler}
+              label="password"
+              errorText="Please enter a valid password address"
             />
-            <Button disabled={!formState}>
-              <span>Login</span>
+
+            <Button type="submit" disabled={!formState.isValid}>
+              {/* {console.log(!formState.isValid)} */}
+              <span>{isLoginMode ? "Login" : "Signup"}</span>
             </Button>
-            <Button inverse onClick={switchModeHandler}>
-              <span>Switch to signup</span>
-            </Button>
+            {/* <Button inverse onClick={switchModeHandler}>
+              <span>
+                Switch to {isLoginMode ? "Signup Mode" : "Login Mode"}
+              </span>
+            </Button> */}
           </form>
         </Card>
       </div>
